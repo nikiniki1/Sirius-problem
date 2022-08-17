f0b0f2c5e147de3ef913ab22f7ba212b97e4165b
Tretyak Ilya <105874587+Tretyak0ID@users.noreply.github.com>
2022-08-17 16:48:46 +0300

    IC

--- initial_conditions.py
+++ initial_conditions.py
@@ -31,25 +31,31 @@ def gaussian_hill_1rotor(domains, h_mean, xc=None, yc=None, sigma_x=100.0, sigma
                 state.u[ind][j,i] =  100*np.sin(np.pi*domain.x[i]/(2 * np.pi * 6371.22 * 1000.0))*np.cos(np.pi*domain.y[j]/(2 * np.pi * 6371.22 * 1000.0))
                 state.v[ind][j,i] = -100*np.cos(np.pi*domain.x[i]/(2 * np.pi * 6371.22 * 1000.0))*np.sin(np.pi*domain.y[j]/(2 * np.pi * 6371.22 * 1000.0))
 
-        dx = (domain.xx - 0.8 * xc) / domain.lx
-        dy = (domain.yy - 0.8 * yc) / domain.ly
+        dx = (domain.xx - 0.5 * xc) / domain.lx
+        dy = (domain.yy - 1.0 * yc) / domain.ly
         state.h[ind] = h_mean + np.exp(-sigma_x * dx ** 2 - sigma_y * dy ** 2)
+
+        dx = (domain.xx - 1.5 * xc) / domain.lx
+        dy = (domain.yy - 1.0 * yc) / domain.ly
+        state.h[ind] += np.exp(-sigma_x * dx ** 2 - sigma_y * dy ** 2)
     return state
 
-def gaussian_hill_2rotor(domains, h_mean, xc=None, yc=None, sigma_x=100.0, sigma_y=100.0):
+def gaussian_hill_2rotor(domains, h_mean, xc=None, yc=None, sigma_x=200.0, sigma_y=200.0):
     state = State.zeros(domains)
 
     xc = xc if xc is not None else domains[0].lx / 2
     yc = yc if yc is not None else domains[0].ly / 2
     for ind, domain in enumerate(domains):
-        for i in range(domain.nx + 1):
-            for j in range(domain.ny + 1):
-                state.u[ind][j,i] =  100*np.sin(np.pi*domain.x[i]/(2 * np.pi * 6371.22 * 1000.0)*2)*np.cos(np.pi*domain.y[j]/(2 * np.pi * 6371.22 * 1000.0))
-                state.v[ind][j,i] = -100*np.cos(np.pi*domain.x[i]/(2 * np.pi * 6371.22 * 1000.0)*2)*np.sin(np.pi*domain.y[j]/(2 * np.pi * 6371.22 * 1000.0))
+        state.u[ind] =  100*np.sin(np.pi*domain.xx/(domain.lx))**2*np.sin(2*np.pi*domain.yy/(domain.ly))
+        state.v[ind] = -100*np.sin(np.pi*domain.yy/(domain.ly))**2*np.sin(2*np.pi*domain.xx/(domain.lx))
 
-        dx = (domain.xx - 1.0 * xc) / domain.lx
+        dx = (domain.xx - 0.5 * xc) / domain.lx
         dy = (domain.yy - 1.0 * yc) / domain.ly
         state.h[ind] = h_mean + np.exp(-sigma_x * dx ** 2 - sigma_y * dy ** 2)
+
+        dx = (domain.xx - 1.5 * xc) / domain.lx
+        dy = (domain.yy - 1.0 * yc) / domain.ly
+        state.h[ind] += np.exp(-sigma_x * dx ** 2 - sigma_y * dy ** 2)
     return state
 
 def geostrophic_balance(domains, pcori, g, h_mean):
